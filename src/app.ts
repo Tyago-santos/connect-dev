20a9b284ffa0f6ce80bcab9a4e93e54e7176be60
Tiago Santos <xlzthyago@gmail.com>
2026-03-18 14:44:13 -0300

    chore: create login controller page and struct

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import helmet from 'helmet';
 import path from 'node:path';
 import mustacheExpress from 'mustache-express';
+import router  from './routes/index.ts';
 
 export const app = express();
 
@@ -15,15 +16,7 @@ app.engine('mustache', mustacheExpress(path.resolve('src/views/partials')));
 app.set('view engine', 'mustache');
 app.set('views', path.resolve('src/views'));
 
-app.get('/', (req, res) => {
-  const title = 'tiago';
-  res.render('pages/home', { title });
-});
-
-app.get('/login', (req, res) => {
-  const title = 'tiago';
-  res.render('pages/login', { title });
-});
+app.use(router);
 
 app.listen(process.env.PORT, () => {
   console.log('servidor rodando');
