6b1c56628506491e77f27965808d7feb73595806
Tiago Santos <xlzthyago@gmail.com>
2026-03-18 15:18:57 -0300

    refect: refctot for dected erro by connect

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,11 +1,18 @@
 import mysql from 'mysql2/promise';
 
-export const connection = mysql.createPool({
-  host: '127.0.0.1',
-  user: 'root',
-  password: '1234',
-  database: 'connect_dev',
-  port: 3306,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+export const connection = async () => {
+  try {
+    return mysql.createPool({
+      host: '127.0.0.1',
+      user: 'root',
+      password: '1234',
+      database: 'connect_dev',
+      port: 3306,
+      waitForConnections: true,
+      connectionLimit: 10,
+    });
+  } catch (error) {
+    console.error('Error ao conectar ao banco de dados:', error);
+    throw error;
+  }
+};
