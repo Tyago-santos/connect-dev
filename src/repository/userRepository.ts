d1da0a3c80bad9f4bfa1788e6a187ad7a285604b
Tiago Santos <xlzthyago@gmail.com>
2026-03-18 17:32:50 -0300

    refect: refect type in repository invalide erro

--- src/repository/userRepository.ts
+++ src/repository/userRepository.ts
@@ -1,24 +1,49 @@
+import type { RowDataPacket } from 'mysql2';
 import { connection } from '../database/connection.ts';
+import type { Pool } from 'mysql2/promise';
 
+interface UserRow extends RowDataPacket {
+  id: number;
+  name: string;
+  password: string;
+  email: string;
+}
+
+export namespace Database {
+  // Interface para a tabela de usuários
+  export interface UserRow extends RowDataPacket {
+    id: number;
+    name: string;
+    password: string;
+    email: string;
+  }
+
+  // Tipo para o Pool (caso queira centralizar)
+  export type Connection = Pool;
+}
 export class UserRepository {
-  private conn: Promise<mysql.Pool>;
+  private conn: Database.Connection;
 
   constructor() {
     this.conn = connection();
   }
   public async getAllUsers() {
     try {
-      const [rows] = await (await this.conn).query('SELECT * FROM  users');
-      console.log(rows);
+      const [rows] = await this.conn.execute('SELECT * FROM  users');
+      return rows;
     } catch (err) {
       console.error(`erro ao pegar usúarios ${err}`);
     }
   }
 
-  public async getUserById(id: number) {
+  public async getUserByEmail(email: string) {
     try {
-      const [rows] = await (await this.conn).query('SELECT * FROM  users WHERE email = ? ', [id]);
-      console.log(rows);
+      const [rows] = await this.conn.execute<Database.UserRow[]>(
+        'SELECT * FROM users WHERE email = ?',
+        [email],
+      );
+
+      return rows[0];
     } catch (err) {
       console.error(`erro ao pegar usúario por id ${err}`);
     }
@@ -28,13 +53,10 @@ export class UserRepository {
     try {
       const [rows] = await (
         await this.conn
-      ).query('INSERT INTO users (name, email, password, birthdate) VALUES (?, ? ,? , ?', [
-        name,
-        password,
-        email,
-        birthdate,
-      ]);
-      console.log(rows);
+      ).query<Database.UserRow[]>(
+        'INSERT INTO users (name, email, password, birthdate) VALUES (?, ? ,? , ?',
+        [name, password, email, birthdate],
+      );
     } catch (err) {
       console.error(`erro ao criar usúario ${err}`);
     }
