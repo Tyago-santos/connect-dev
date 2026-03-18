f2f9d022554d986a685d5c3b5f54ebd713366ce2
Tiago Santos <xlzthyago@gmail.com>
2026-03-18 15:34:55 -0300

    update files app package

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,22 @@ import express from 'express';
 import helmet from 'helmet';
 import path from 'node:path';
 import mustacheExpress from 'mustache-express';
-import router  from './routes/index.ts';
+import router from './routes/index.ts';
+import session from 'express-session';
+import flash from 'connect-flash';
 
 export const app = express();
 
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(
+  session({
+    secret: 'your-secret-key',
+    resave: false,
+    saveUninitialized: false,
+  }),
+);
+app.use(flash());
 
 app.use(express.static(path.resolve('public')));
 
