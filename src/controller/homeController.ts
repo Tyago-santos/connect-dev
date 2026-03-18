7633d586a56d9846e2f0a7d963b370ed9614d707
Tiago Santos <xlzthyago@gmail.com>
2026-03-18 18:16:37 -0300

    chore: implement user infor in home

--- src/controller/homeController.ts
+++ src/controller/homeController.ts
@@ -1,11 +1,18 @@
 import type { Request, Response } from 'express';
 import { UserRepository } from '../repository/userRepository.ts';
+import type { email } from 'zod';
 
 export default class HomeController {
   private repository = new UserRepository();
 
   public index = (req: Request, res: Response) => {
-    this.repository.getUsers();
-    res.render('pages/home', { title: 'Home' });
+    res.render('pages/home', {
+      title: 'Home',
+      user: {
+        name: req.session.user?.name,
+        email: req.session.user?.email,
+        id: req.session.user?.id,
+      },
+    });
   };
 }
