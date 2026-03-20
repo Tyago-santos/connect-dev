1cd1f0c3996fd0dbd729dd8cb8650d27f95fad53
Tiago Santos <xlzthyago@gmail.com>
2026-03-20 17:51:11 -0300

    fix: fix erro database

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -2,8 +2,16 @@ import mysql from 'mysql2/promise';
 
 export const connection = () => {
   try {
-    const dbUrl = process.env.DATABASE_URL || 'mysql://root:1234@127.0.0.1:3306/connec_dev';
-    return mysql.createPool(dbUrl);
+ 
+    return mysql.createPool({
+      host: '127.0.0.1',
+      user: 'root',
+      password: '1234',
+      database: 'connect_dev',
+      port: 3306,
+      waitForConnections: true,
+      connectionLimit: 10,
+    });
   } catch (error) {
     console.error('Error ao conectar ao banco de dados:', error);
     throw error;
