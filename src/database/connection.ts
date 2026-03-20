fe064b6dabded8c71c8d34e4ea3d63464711895a
Tiago Santos <xlzthyago@gmail.com>
2026-03-20 18:15:30 -0300

    fix: fix database mysql

--- src/database/connection.ts
+++ src/database/connection.ts
@@ -1,17 +1,18 @@
 import mysql from 'mysql2/promise';
 
+export const dbConfig = {
+  host: '127.0.0.1',
+  user: 'root',
+  password: '1234',
+  database: 'connect_dev',
+  port: 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+};
+
 export const connection = () => {
   try {
- 
-    return mysql.createPool({
-      host: '127.0.0.1',
-      user: 'root',
-      password: '1234',
-      database: 'connect_dev',
-      port: 3306,
-      waitForConnections: true,
-      connectionLimit: 10,
-    });
+    return mysql.createPool(dbConfig);
   } catch (error) {
     console.error('Error ao conectar ao banco de dados:', error);
     throw error;
